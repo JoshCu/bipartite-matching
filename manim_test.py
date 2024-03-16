8ff6fea693e95942b2f5fd3ce68b371509627923
Josh Cunningham <josh.cu@gmail.com>
2024-03-16 15:11:25 -0500

    changed to make bfs graphs using networkx

--- manim_test.py
+++ manim_test.py
@@ -3,6 +3,8 @@ from copy import copy
 from utils import load_file, matrix_to_edge_pairs
 from collections import defaultdict
 
+import networkx as nx
+
 graph_name = "size3.graph"
 anim_step_file = graph_name + ".anim"
 
@@ -44,36 +46,32 @@ def create_graph_from_adj_matrix(adj_matrix):
     return graph
 
 
-def create_graph_from_edges(unnamed_edges):
-    all_edges = defaultdict(set)
-
-    for e in unnamed_edges:
-        u_e = get_node_name(e[0], True)
-        v_e = get_node_name(e[1], False)
-        all_edges[u_e].add(v_e)
-
+def create_graph_from_edges(unnamed_edges, free_nodes, match_edges):
+    free_nodes = [get_node_name(node, True) for node in free_nodes]
+    edges = [(get_node_name(u, True), get_node_name(v, False)) for u, v in unnamed_edges]
+    matched_edges = [(get_node_name(v, False), get_node_name(u, True)) for u, v in match_edges]
     graphs = VGroup()
-    for u_node, v_nodes in all_edges.items():
-        v_nodes = sorted(list(v_nodes))
+    edges += matched_edges
+    # for each free node, create a graph
+    reference_graph = nx.DiGraph(edges)
+    for root in free_nodes:
 
-        # two lists below determine drawing order
-        node_edges = [(u_node, v) for v in v_nodes]
-        unique_nodes = [u_node] + v_nodes
-
-        partitions = [[u_node], v_nodes]
-
+        subgraph = nx.ego_graph(reference_graph, root, radius=len(reference_graph.nodes))
+        nodes = list(subgraph.nodes)
+        edges = list(subgraph.edges)
+        print(f"root: {root}, nodes: {nodes}, edges: {edges}")
         node_config = {}
-        for node in unique_nodes:
-            node_config[node] = {"fill_color": GREEN, "radius": NODE_RADIUS}
-        node_config[u_node] = {"fill_color": BLUE, "radius": NODE_RADIUS}
-
+        for node in nodes:
+            if node.startswith("u"):
+                node_config[node] = {"fill_color": GREEN, "radius": NODE_RADIUS}
+            else:
+                node_config[node] = {"fill_color": BLUE, "radius": NODE_RADIUS}
         graph = Graph(
-            unique_nodes,
-            node_edges,
-            layout="tree",
-            partitions=partitions,
+            nodes,
+            edges,
+            # layout="tree",
             vertex_config=node_config,
-            root_vertex=u_node,
+            root_vertex=root,
             edge_config=EDGE_CONFIG,
         )
         graphs.add(graph)
@@ -105,18 +103,14 @@ class BipartiteGraphAnimation(Scene):
 
         self.next_section("BFS Graphs")
         while True:
+            print(anim_steps)
             bfs_edges = eval(anim_steps.pop(0).split(":")[1])
             matched_nodes = eval(anim_steps.pop(0).split(":")[1])
             edge_remove = eval(anim_steps.pop(0).split(":")[1])
             free_nodes = eval(anim_steps.pop(0).split(":")[1])
 
-            for e in edge_remove:
-                # set the edge to be removed to red then back to grey
-                u = get_node_name(e[0], True)
-                v = get_node_name(e[1], False)
-                self.play(G.edges[(u, v)].animate.set_color(RED), run_time=0.5)
-                self.wait(1)
-                self.play(G.edges[(u, v)].animate.set_color(GRAY), run_time=0.5)
+            # remove matched nodes from bfs_edges
+            bfs_edges = [edge for edge in bfs_edges if edge not in matched_nodes]
 
             free_n_highlight = {}
             indicate_free_nodes = []
@@ -133,9 +127,10 @@ class BipartiteGraphAnimation(Scene):
             self.play(
                 *[Create(circle) for circle in free_n_highlight.values()], *indicate_free_nodes
             )
+
             self.next_section("Adding BFS Trees")
             self.wait(1)
-            bfs_graphs = create_graph_from_edges(bfs_edges)
+            bfs_graphs = create_graph_from_edges(bfs_edges, free_nodes, matched_nodes)
             # arrange and scale to fit on screen
             bfs_graphs.arrange_in_grid(buff=0.3)
             # get the scale factor to fit the available width
@@ -165,6 +160,14 @@ class BipartiteGraphAnimation(Scene):
                     )
                     self.play(Indicate(root), FadeTransform(circle, root))
             self.wait(1)
+
+            # set the edge to be removed to red then back to grey
+            if type(edge_remove) == tuple:
+                u = get_node_name(edge_remove[0], True)
+                v = get_node_name(edge_remove[1], False)
+                self.play(G.edges[(u, v)].animate.set_color(RED), run_time=0.5)
+                self.wait(1)
+                self.play(G.edges[(u, v)].animate.set_color(GRAY), run_time=0.5)
 
             self.next_section("Matching Edges")
 
@@ -204,15 +207,23 @@ class BipartiteGraphAnimation(Scene):
                     if len(g.vertices) == 1:
                         objects_to_remove.append(g)
                         bfs_graphs.remove(g)
-
-                self.play(*[FadeOut(o) for o in objects_to_remove])
-                # remove the objects from the scene
-                self.remove(*objects_to_remove)
+                if len(objects_to_remove) > 0:
+                    self.play(*[FadeOut(o) for o in objects_to_remove])
+                    # remove the objects from the scene
+                    self.remove(*objects_to_remove)
                 self.wait(1)
 
-            break
+            if not anim_steps[0].startswith("bfs_edges"):
+                break
 
         # add the bfs graph
 
         # Hold the final state for a moment before closing
         self.wait(1)
+
+        # move the graph back to the center
+        self.play(G.animate.move_to(ORIGIN))
+        self.wait(1)
+
+        # remove edges set to GRAY
+        self.play(*[FadeOut(e) for _, e in G.edges.items() if e.get_color() == GRAY])
