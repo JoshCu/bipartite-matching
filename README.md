5d534a49b6149a0af10bba63810b5171fa4f8e50
Josh Cunningham <josh.cu@gmail.com>
2024-03-17 16:46:37 -0500

    Rename manim file and improve algorithm comments

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 to run and render
 ```bash
 python hopkarp.py > size4.graph.anim
-manim -plq manim_test.py BipartiteGraphAnimation
+manim -plq manimate.py BipartiteGraphAnimation
 ```
 
-the graph input selection is hard coded in both hopkarp.py and manim_test.py   
+the graph input selection is hard coded in both hopkarp.py and manimate.py   
 shameful, but temporary  
 
 ## Requirements
 * manim
 * latex
 
-*to run without latex, set `labels=False` in the graphs in manim_test.py 
+*to run without latex, set `labels=False` in the graphs in manimate.py 
--- hopkarp.py
+++ hopkarp.py
@@ -1,21 +1,27 @@
+# Description: Implementation of the Hopcroft-Karp algorithm for finding the maximum matching in a bipartite graph
+# It makes use of an additional dummy node connected to all unmatched nodes in the left set
+# This all makes it easier to find augmenting paths
+
+
 from queue import Queue
 from math import inf as INFINITY
-from utils import load_file, matrix_to_adj_list
-
-import json
+from utils import load_file, matrix_to_adj_list, parse_args
 
 DUMMY_NODE = -1
+
+# left nodes are u
+# right nodes are v
 
 
 class BipGraph(object):
     """
     Class to represent a bipartite graph
-    dim_m: number of left nodes
-    dim_n: number of right nodes
-    adjacency_hash: dict containing an adjacency list of the bipartite graph
-    u_to_v_map: dict containing the matching of left nodes to right nodes
-    v_to_u_map: dict containing the matching of right nodes to left nodes
-    distance_to_dummy: dict containing the distance of each left node to the dummy node
+    dim_u: number of u nodes
+    dim_v: number of v nodes
+    adj_lookup: dict containing an adjacency list of the bipartite graph
+    u_to_v_map: dict containing the matching of u nodes to v nodes
+    v_to_u_map: dict containing the matching of v nodes to u nodes
+    distance_to_dummy: dict containing the distance of each u node to the dummy node
     """
 
     def __init__(self, adj_matrix):
@@ -23,9 +29,9 @@ class BipGraph(object):
         :param adj_matrix: The adjacency matrix of the bipartite graph
         """
 
-        self.dim_m = len(adj_matrix)
-        self.dim_n = len(adj_matrix[0])
-        self.adjacency_hash = matrix_to_adj_list(adj_matrix)
+        self.dim_u = len(adj_matrix)
+        self.dim_v = len(adj_matrix[0])
+        self.adj_lookup = matrix_to_adj_list(adj_matrix)
 
         self.bfs_edges = []
 
@@ -34,10 +40,10 @@ class BipGraph(object):
         self.distance_to_dummy = {}
 
         # initalize all pairs to connect to the dummy node
-        for u in range(self.dim_m):
+        for u in range(self.dim_u):
             self.u_to_v_map[u] = DUMMY_NODE
             self.distance_to_dummy[u] = DUMMY_NODE
-        for v in range(self.dim_n):
+        for v in range(self.dim_v):
             self.v_to_u_map[v] = DUMMY_NODE
 
     def bfs(self):
@@ -46,9 +52,10 @@ class BipGraph(object):
         """
         self.bfs_edges = []
         Q = Queue()
-        for u in range(self.dim_m):
+        for u in range(self.dim_u):
             # if the node is paired to the dummy (aka unmatched)
             if self.u_to_v_map[u] == DUMMY_NODE:
+                # set the distance to the dummy node to 0 and add it to the queue
                 self.distance_to_dummy[u] = 0
                 Q.put(u)
             else:
@@ -58,16 +65,18 @@ class BipGraph(object):
         # variable to keep track of the tree created
         while not Q.empty():
             u = Q.get()
-            # see if we can find a shorter path to the dummy node
+            # if u is less than the distance to the dummy node
             if self.distance_to_dummy[u] < self.distance_to_dummy[DUMMY_NODE]:
-                # for all right nodes adjacent to left node u
-                for v in self.adjacency_hash[u]:
-                    # if the right node is unmatched
+                # for each v connected to u
+                for v in self.adj_lookup[u]:
+                    # add the edge to the tree
                     self.bfs_edges.append((u, v))
+                    # if the distance to the dummy node is infinity
                     if self.distance_to_dummy[self.v_to_u_map[v]] == INFINITY:
                         # we found an augmenting path, update the distance and add the right node to the queue
                         self.distance_to_dummy[self.v_to_u_map[v]] = self.distance_to_dummy[u] + 1
                         Q.put(self.v_to_u_map[v])
+        # if the distance to the dummy node is not infinity then a path to the dummy node was found
         augmenting_path_found = self.distance_to_dummy[DUMMY_NODE] != INFINITY
         return augmenting_path_found
 
@@ -78,8 +87,8 @@ class BipGraph(object):
         if u == DUMMY_NODE:
             # if we reach the dummy node, we found an augmenting path
             return True
-        # get all nodes adjacent to and see if we can find an augmenting path
-        for v in self.adjacency_hash[u]:
+        # get all nodes adjacent to u and see if we can find an augmenting path
+        for v in self.adj_lookup[u]:
             if self.distance_to_dummy[self.v_to_u_map[v]] == self.distance_to_dummy[u] + 1:
                 # recursively search deeper for an augmenting path
                 if self.dfs(self.v_to_u_map[v]):
@@ -95,6 +104,7 @@ class BipGraph(object):
     def find_matches_hopcroft_karp(self):
         matches = 0
         # while there is an augmenting path to be found
+        # perform a bfs on all the unmatched nodes
         while self.bfs():
             print(f"bfs_edges: {repr(self.bfs_edges)}")
             matched_nodes = [(u, v) for u, v in self.u_to_v_map.items() if v != DUMMY_NODE]
@@ -106,7 +116,7 @@ class BipGraph(object):
             print(f"edge remove: {repr(edge_to_augment)}")
             unmatched_nodes = [u for u, v in self.u_to_v_map.items() if v == DUMMY_NODE]
             print(f"unmatched nodes: {repr(unmatched_nodes)}")
-            for u in range(self.dim_m):
+            for u in range(self.dim_u):
                 # if the node is free and we found an augmenting path
                 if self.u_to_v_map[u] == DUMMY_NODE and self.dfs(u):
                     matches += 1
@@ -116,6 +126,6 @@ class BipGraph(object):
 
 
 if __name__ == "__main__":
-    adj_matrix = load_file("size4.graph")
+    adj_matrix = load_file(parse_args())
     g = BipGraph(adj_matrix)
     print("Size of maximum matching is %d" % g.find_matches_hopcroft_karp())
